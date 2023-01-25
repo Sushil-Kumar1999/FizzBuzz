32dfc7a2814c93fa0514676ca8e70ab212cc35a8
Sushil Kumar <sushil.kumar2@apexon.com>
2023-01-25 11:29:37 +0000

    changed dependency lifetime to scoped

--- FizzBuzz/Registrations.cs
+++ FizzBuzz/Registrations.cs
@@ -7,12 +7,12 @@ public static class Registrations
 {
     public static void RegisterServices(HostBuilderContext hostContext, IServiceCollection services)
     {
-        services.AddTransient<IFactory, FizzBuzzFactory>();
-        services.AddTransient<IFlow, FizzBuzzFlow>();
-        services.AddTransient<IConsole, FizzBuzzConsole>();
-        services.AddTransient<IGenerator, FizzBuzzGenerator>();
-        services.AddTransient<IInputMapper, FizzBuzzInputMapper>();
-        services.AddTransient<IInputValidator, FizzBuzzInputValidator>();
-        services.AddTransient<IOutputBuilder, FizzBuzzOutputBuilder>();
+        services.AddScoped<IFactory, FizzBuzzFactory>();
+        services.AddScoped<IFlow, FizzBuzzFlow>();
+        services.AddScoped<IConsole, FizzBuzzConsole>();
+        services.AddScoped<IGenerator, FizzBuzzGenerator>();
+        services.AddScoped<IInputMapper, FizzBuzzInputMapper>();
+        services.AddScoped<IInputValidator, FizzBuzzInputValidator>();
+        services.AddScoped<IOutputBuilder, FizzBuzzOutputBuilder>();
     }
 }
